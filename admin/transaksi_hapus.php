9b75e5f328071a210a91ed98d852663a9c45069c
AZMII HANIIFAH <168162631+azmiihaniif@users.noreply.github.com>
2025-06-13 22:11:49 +0700

    Update transaksi_hapus.php

--- admin/transaksi_hapus.php
+++ admin/transaksi_hapus.php
@@ -1,19 +1,17 @@
 <?php
-//from transaksi.php
-//menghubungkan koneksi
 include '../koneksi.php';
 
-//menangkap data id yang dikirim melalui url
 $id = $_GET['id'];
 
-//menghapus transaksi
-mysqli_query($koneksi, "DELETE FROM transaksi WHERE 
-transaksi_id='$id'");
+// Hapus dulu data di log_transaksi yang terkait dengan transaksi ini
+mysqli_query($koneksi, "DELETE FROM log_transaksi WHERE log_transaksi_id='$id'");
 
-//menghapus data pakaian dalam transaksi ini
-mysqli_query($koneksi, "DELETE FROM pakaian WHERE 
-pakaian_transaksi='$id'");
+// Hapus dulu data di pakaian yang terkait dengan transaksi ini
+mysqli_query($koneksi, "DELETE FROM pakaian WHERE pakaian_transaksi='$id'");
 
-//alihkan halaman ke halaman transaksi
+// Baru hapus data transaksi
+mysqli_query($koneksi, "DELETE FROM transaksi WHERE transaksi_id='$id'");
+
+// Redirect kembali ke halaman transaksi
 header("location:transaksi.php");
 ?>
