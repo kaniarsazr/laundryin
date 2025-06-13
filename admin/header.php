b3bb94b57c094be89193d2f7492ee71ed4c325fb
Febrianadella <153357442+Febrianadella@users.noreply.github.com>
2025-06-13 11:52:08 +0700

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -1,28 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
     <title>Sistem Informasi Laundry</title>
-    <link rel="stylesheet" type="text/css" 
-    href="../assets/css/bootstrap.css">
-    <script type="text/javascript" 
-    src="../assets/js/jquery.js"></script>
-    <script type="text/javascript" 
-    src="../assets/js/bootstrap.js"></script>
-
-    <link rel="stylesheet" type="text/css" 
-    href="../assets/font-awesome/css/all.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+    <script type="text/javascript" src="../assets/js/jquery.js"></script>
+    <script type="text/javascript" src="../assets/js/bootstrap.js"></script>
+    <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/all.css">
 </head>
 
 <body style="background: #f0f0f0">
-    <!--Cek apakah sudah login-->
+
+    <!-- Cek apakah sudah login -->
     <?php
         session_start();
-        if($_SESSION['status'] != "login"){
+        if ($_SESSION['status'] != "login") {
             header("location:../index.php?pesan=belum_login");
         }
     ?>
 
-    <!--Menu Navigasi-->
+    <!-- Menu Navigasi -->
     <nav class="navbar navbar-inverse" style="border-radius: 0px">
         <div class="container-fluid">
             <div class="navbar-header">
@@ -34,37 +31,30 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php">
-                LaundryIn</a>
-            </div><!--End of "navbar-header"-->
+                <a class="navbar-brand" href="index.php">LaundryIn</a>
+            </div><!-- End of navbar-header -->
 
-            <div class="collapse navbar-collapse" 
-            id="#bs-example-navbar-collapse-1">
+            <div class="collapse navbar-collapse" id="#bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="index.php">
-                    <i class="glyphicon glyphicon-home">
-                    </i>Dashboard</a></li><!--HOME-->
+                    <i class="glyphicon glyphicon-home"></i>Dashboard</a></li>
                     <li><a href="pelanggan.php">
-                    <i class="glyphicon glyphicon-user">
-                    </i>Pelanggan</a></li><!--PELANGGAN-->
+                    <i class="glyphicon glyphicon-user"></i>Pelanggan</a></li>
                     <li><a href="transaksi.php">
-                    <i class="glyphicon glyphicon-random">
-                    </i>Transaksi</a></li><!--TRANSAKSI-->
+                    <i class="glyphicon glyphicon-random"></i>Transaksi</a></li>
                     <li><a href="laporan.php">
-                    <i class="glyphicon glyphicon-list-alt">
-        
-                    
+                    <i class="glyphicon glyphicon-list-alt"></i>Laporan</a></li>
                     <li><a href="logout.php">
-                    <i class="glyphicon glyphicon-log-out">
-                    </i>Log Out</a></li>
-                </ul> <!--nav navbar-nav-->
+                    <i class="glyphicon glyphicon-log-out"></i>Log Out</a></li>
+                </ul> <!-- End of nav navbar-nav -->
 
                 <ul class="nav navbar-nav navbar-right">
                     <li><p class="navbar-text"><i class="fa fa-user-shield"></i> Halo, <b>
-                    <?php echo $_SESSION['username'];?></b>!</p></li>
+                    <?php echo $_SESSION['username']; ?></b>!</p></li>
                 </ul>
-            </div> <!--End of 'collapse navbar-collapse'-->
-        </div><!--End of "Container-fluid"-->
+            </div> <!-- End of collapse navbar-collapse -->
+        </div> <!-- End of container-fluid -->
     </nav>
+
 </body>
 </html>
