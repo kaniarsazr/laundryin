d11f779ea26bd298bb039efc3ab07e257f06a34e
Febrianadella <153357442+Febrianadella@users.noreply.github.com>
2025-06-13 15:22:25 +0700

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -1,95 +1,99 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Sistem Informasi Laundry</title>
-    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/all.css">
-    <script type="text/javascript" src="../assets/js/jquery.js"></script>
-    <script type="text/javascript" src="../assets/js/bootstrap.js"></script>
+  <meta charset="UTF-8">
+  <title>Sistem Informasi Laundry - LaundryIn</title>
 
-    <!-- Google Font Poppins -->
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
-    <style>
-        body {
-            background: #f0f0f0;
-            font-family: 'Poppins', sans-serif;
-        }
+  <!-- Remixicon (ikon tambahan jika diperlukan) -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.min.css" />
 
-        .navbar {
-            background: linear-gradient(to right, #0275d8, #1891f5, #5bc0de);
-            border: none;
-            border-radius: 0;
-            box-shadow: 0 2px 6px rgba(0,0,0,0.05);
-        }
+  <!-- Custom Styling -->
+  <style>
+    body {
+      background: #f0f0f0;
+      font-family: 'Arial', sans-serif;
+    }
 
-        .navbar-brand {
-            color: #fff !important;
-            font-weight: 600;
-            font-size: 22px;
-        }
+    .navbar {
+      background: linear-gradient(to right, #0275d8, #1891f5, #5bc0de);
+      border: none;
+      border-radius: 0;
+    }
 
-        .navbar-nav > li > a {
-            color: #fff !important;
-            font-weight: 500;
-        }
+    .navbar-brand,
+    .navbar-nav > li > a,
+    .navbar-text {
+      color: #fff !important;
+      font-weight: 500;
+    }
 
-        .navbar-nav > li > a:hover,
-        .navbar-nav > .active > a {
-            background-color: rgba(255, 255, 255, 0.2) !important;
-            border-radius: 5px;
-        }
+    .navbar-nav > li > a:hover,
+    .navbar-nav > .active > a {
+      background-color: rgba(255, 255, 255, 0.2) !important;
+      border-radius: 5px;
+    }
 
-        .navbar-text {
-            color: #fff !important;
-            font-weight: 500;
-        }
+    .container-content {
+      max-width: 1000px;
+      margin: 30px auto;
+      background: white;
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: 0px 2px 8px rgba(0,0,0,0.1);
+    }
+  </style>
 
-        .navbar-toggle .icon-bar {
-            background-color: #fff;
-        }
-    </style>
+  <!-- jQuery dan Bootstrap JS -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
-
 <?php
-    session_start();
-    if ($_SESSION['status'] != "login") {
-        header("location:../index.php?pesan=belum_login");
-    }
+session_start();
+if ($_SESSION['status'] != "login") {
+    header("location:../index.php?pesan=belum_login");
+}
 ?>
 
 <nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" 
-                    data-toggle="collapse" data-target="#bs-navbar"
-                    aria-expanded="false">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.php">LaundryIn</a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="bs-navbar">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="index.php"><i class="glyphicon glyphicon-home"></i> Dashboard</a></li>
-                <li><a href="pelanggan.php"><i class="glyphicon glyphicon-user"></i> Pelanggan</a></li>
-                <li><a href="transaksi.php"><i class="glyphicon glyphicon-random"></i> Transaksi</a></li>
-                <li><a href="laporan.php"><i class="glyphicon glyphicon-list-alt"></i> Laporan</a></li>
-                <li><a href="logout.php"><i class="glyphicon glyphicon-log-out"></i> Log Out</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <p class="navbar-text">
-                        <i class="fa fa-user-shield"></i> Halo, <b><?php echo $_SESSION['username']; ?></b>!
-                    </p>
-                </li>
-            </ul>
-        </div>
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed"
+              data-toggle="collapse"
+              data-target="#bs-example-navbar-collapse-1"
+              aria-expanded="false">
+        <span class="sr-only">Toggle Navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">LaundryIn</a>
     </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php"><i class="glyphicon glyphicon-home"></i> Dashboard</a></li>
+        <li><a href="pelanggan.php"><i class="glyphicon glyphicon-user"></i> Pelanggan</a></li>
+        <li><a href="transaksi.php"><i class="glyphicon glyphicon-random"></i> Transaksi</a></li>
+        <li><a href="laporan.php"><i class="glyphicon glyphicon-list-alt"></i> Laporan</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown"
+             role="button" aria-haspopup="true" aria-expanded="false">
+             <i class="glyphicon glyphicon-wrench"></i> Pengaturan <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="harga.php"><i class="glyphicon glyphicon-usd"></i> Pengaturan Harga</a></li>
+            <li><a href="ganti_password.php"><i class="glyphicon glyphicon-lock"></i> Ganti Password</a></li>
+          </ul>
+        </li>
+        <li><a href="logout.php"><i class="glyphicon glyphicon-log-out"></i> Log Out</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><p class="navbar-text"><i class="glyphicon glyphicon-user"></i> Halo, <b><?php echo $_SESSION['username']; ?></b>!</p></li>
+      </ul>
+    </div>
+  </div>
 </nav>
